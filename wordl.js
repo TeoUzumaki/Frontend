5a797ed670ef0f4f807d3d352499a0ebeb7c4979
TeoUzumaki <teowkeys@gmail.com>
2025-06-12 21:03:13 +0100

    Update wordl.js

--- wordl.js
+++ wordl.js
@@ -2,7 +2,7 @@
 let WORD_LIST = [];
 let VALID_WORDS = [];
 // === CHOOSE YOUR DAILY ANSWER HERE! (must be 5-letter UPPERCASE and in the txt file) ===
-let ANSWER = "CODES";; // <-- Change this for your daily answer!
+let ANSWER = "GIFTS";; // <-- Change this for your daily answer!
 
 let guesses = [];
 let currentGuess = "";
